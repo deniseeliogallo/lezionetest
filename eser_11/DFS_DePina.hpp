1623c714e1f4348a70d984866e7d25a0d9bf5c38
deniseeliogallo <eliodengallo@gmail.com>
2026-06-18 17:36:19 +0000

    consegna dfs e de pina

--- eser_11/DFS_DePina.hpp
+++ eser_11/DFS_DePina.hpp
@@ -1,17 +1,21 @@
 #pragma once
+
 #include <iostream>
 #include <concepts>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
+#include <algorithm>
+
 #include "grafo.hpp"
 #include "Dijkstra.hpp"
 
-//implemento il codice del file "cicli fondamentali di un grafo"
+
 template <typename T> requires std::integral<T>   
 bool findpath(const unidirected_graph<T>& Tree, T u, T v, std::set<T>& visited, std::vector<T>& path) {
-    visited.insert(u);
+    //segno come visitato e inserisco nel cammino u
+	visited.insert(u);
     path.push_back(u);
     
     if (u == v) return true;
@@ -19,120 +23,146 @@ bool findpath(const unidirected_graph<T>& Tree, T u, T v, std::set<T>& visited, 
     for (const auto& n : Tree.neighbors(u)) { 
 	// uso il metodo find(n) che cerca un elemento con chiave uguale ad n, se nessun elemento è trovato, restituisce l'iteratore past-the-end (end())
         if (visited.find(n) == visited.end()) {
-            if (findpath(Tree, n, v, visited, path)) { 
+            if (findpath(Tree, n, v, visited, path)) {  //verifico che il nodo vicino n arrivi a v tramite la chiamata ricorsiva
                 return true;
             }
         }
-    }
-    
-    path.pop_back();
+    }  
+    path.pop_back(); //se invece non trova il cammino rimuove u da path 
     return false;
 }
 
 // DFS
 template <typename T> requires std::integral<T> 
 std::vector<T> DFS_cycle(const unidirected_graph<T>& Tree, T nodoA, T nodoB){
-    std::vector<T> path; //potevo usare anche una lista, però ho usato un vettore perchè occupa meno memoria ed è piu facilmente accessibile
+    std::vector<T> path; 
     std::set<T> visited;
 
     if (findpath(Tree, nodoA, nodoB, visited, path)) {
-        path.push_back(nodoA);
+        path.push_back(nodoA); //chiudiamo il ciclo
     }
     
     return path;    
 }
 
+
+
 // Algoritmo di De Pina 
 template <typename T> requires std::integral<T>
-std::vector<std::vector<bool>> De_Pina (const unidirected_graph<T>& G, std::vector<std::vector<bool>> S, size_t k, const std::map<unidirected_edge<T>, int>& pesi_G){
+std::vector<std::vector<bool>> De_Pina (const unidirected_graph<T>& G, std::vector<std::vector<bool>> S, size_t k){
     
     size_t m = G.all_edges().size();
+	size_t cost = G.all_nodes().size();
+	
+	//C_i vettori booleani che rappresentano gli archi dei cicli: i=1  se l'arco fa parte del ciclo se no è zero
     std::vector<std::vector<bool>> C(k, std::vector<bool>(m, false));
     
     for (size_t i = 0; i < k; i++){
-        unidirected_graph<T> G_primo;
-        std::map<unidirected_edge<T>, int> pesi_G_primo;
+		
+		unidirected_graph<T> G_primo;
+		std::map<unidirected_edge<T>, double> pesi_G_primo;
     
         for(const auto& arco : G.all_edges()){
             T u = arco.from();
             T v = arco.to();
-			
-            //per vedere una distinzione tra i nodi di G e i nodi di G', ho pensato di dividere i nodi tra pari in G e dispari in G'
-			T u_piu  = 2 * u;
-            T u_piu  = 2 * u;
-            T u_meno = 2 * u + 1;
-            T v_piu  = 2 * v;
-            T v_meno = 2 * v + 1;
+		
+			// divido i nodi in positivi e negativi aggiungendo a u e v il fattore costante
+            T u_piu  = u;
+            T u_meno = u + cost;
+            T v_piu  = v;
+            T v_meno = v + cost;
             
             int num = G.edge_number(arco);
-            int peso_arco = pesi_G.at(arco);
             
             if (S[i][num] == true) {
                 // Se è attivo: aggiungo a G' (u+, v-) e (u-, v+)
                 G_primo.add_edge(u_piu, v_meno);
                 G_primo.add_edge(u_meno, v_piu);
             
-                pesi_G_primo[{u_piu, v_meno}] = peso_arco;
-                pesi_G_primo[{u_meno, v_piu}] = peso_arco;
+                pesi_G_primo[{u_piu, v_meno}] = 1;
+                pesi_G_primo[{u_meno, v_piu}] = 1;
             } 
             else {
                 // Se no aggiungo (u+, v+) e (u-, v-)
                 G_primo.add_edge(u_piu, v_piu);
                 G_primo.add_edge(u_meno, v_meno);
                 
-                pesi_G_primo[{u_piu, v_piu}] = peso_arco;
-                pesi_G_primo[{u_meno, v_meno}] = peso_arco;
+                pesi_G_primo[{u_piu, v_piu}] = 1;
+                pesi_G_primo[{u_meno, v_meno}] = 1;
             }
         }
-            
-        std::vector<T> cammino_minimo_scelto;
-        int min_peso_assoluto = std::numeric_limits<int>::max();
         
+		//inizializzo C_i e min_elementi al massimo intero di macchina
+		std::vector<bool> C_i(m, false);		
+        int min_elementi = std::numeric_limits<int>::max();
+		
         for(const auto& nodi : G.all_nodes()){
-            T v_piu  = 2 * nodi;
-            T v_meno = 2 * nodi + 1;
+			//chiamiamo dijkstra per trovare i cammini minimi
+			
+            T v_piu  = nodi;
+            T v_meno = nodi + cost;
             
-            unidirected_graph<T> albero = dijkstra(G_primo, v_meno, pesi_G_primo);
+			std::vector<double> distanze;
+			std::vector<T> pred;
+			
+			dijkstra(2 * cost + 1, G_primo, v_piu, pesi_G_primo, distanze, pred);
             
-            std::vector<T> cammino_minimo;
-            std::set<T> visitati;
-            bool percorso = findpath(albero, v_meno, v_piu, visitati, cammino_minimo);
-            
-            // faccio un controllo per evitare crash del programma in caso percorso è false
-            if (percorso && cammino_minimo.size() > 1) {
-                int peso_cammino_corrente = 0;
-                for (size_t p = 0; p < cammino_minimo.size() - 1; ++p) {
-                    unidirected_edge<T> arco_path(cammino_minimo[p], cammino_minimo[p+1]);
-                    peso_cammino_corrente += pesi_G_primo.at(arco_path);
-                }
-                
-                if (peso_cammino_corrente < min_peso_assoluto) {
-                    min_peso_assoluto = peso_cammino_corrente;
-                    cammino_minimo_scelto = cammino_minimo;
-                }
-            }
-        }
-        
-        
-        if (!cammino_minimo_scelto.empty()) {
-            for (size_t j = 0; j < cammino_minimo_scelto.size() - 1; ++j) {
-                T nodo1_primo = cammino_minimo_scelto[j];
-                T nodo2_primo = cammino_minimo_scelto[j+1];
-                
-                T u_orig = nodo1_primo / 2;
-                T v_orig = nodo2_primo / 2;
-                
-                unidirected_edge<T> arco_originale(u_orig, v_orig);
-                int num = G.edge_number(arco_originale);
-                
-				// Incrementiamo modulo 2
-                C[i][num] = !C[i][num]; 
-      
-            }
-        }
+			//le distanze sono inizializzate a infinito - dunque controllo se è stato trovato un percorso oppure no
+            if (distanze[v_meno] != std::numeric_limits<double>::infinity()) {
+				
+				//Questo vettore rappresenterà il cammino corrente
+				std::vector<bool> C_mu(m, false);
+				
+				// partiamo dal nodo di arrivo e si risale all'indietro.
+				T nodo_corrente = v_meno;
+				
+				while (nodo_corrente != v_piu) {
+					T padre = pred[nodo_corrente];
+				
+					//ritroviamo i nodi originali del grafo G
+					T u_orig;
+					if (padre > (T)cost) {
+						u_orig = padre - cost;
+					} else {
+						u_orig = padre;
+					}
+
+					T v_orig;
+					if (nodo_corrente > (T)cost) {
+						v_orig = nodo_corrente - cost;
+					} else {
+						v_orig = nodo_corrente;
+					}
+					
+					//troviamo la posizione dell'arco originale
+					unidirected_edge<T> arco_originale(u_orig, v_orig);
+					int num = G.edge_number(arco_originale);
+				
+					//se esiste incrementiamo modulo 2 l'elemento di C_mu relativo all'arco
+					if(num != -1){
+						C_mu[num] = !C_mu[num];
+					}
+				
+					nodo_corrente = padre;
+				}
+				
+				//contiamo quanti archi sono attivati ('true') e se ha meno elementi di C_i aggiorniamo C_i
+				int count = std::count(C_mu.begin(), C_mu.end(), true);
+				
+				if (count < min_elementi) {
+					min_elementi = count;
+					C_i = C_mu;
+				}
+				
+			}
+		}
+		
+		// salviamo il ciclo minimo C_i
+        C[i] = C_i;
+       
         
         for (size_t j = i + 1; j < k; ++j) {
-            // Calcolo il prodotto scalare <C_i, S_j> modificato modulo 2
+            // Calcolo il prodotto scalare <C_i, S_j> 
             size_t prodscalare = 0;
             for (size_t id = 0; id < m; id++) {
                 if (C[i][id] && S[j][id]) {
@@ -140,7 +170,7 @@ std::vector<std::vector<bool>> De_Pina (const unidirected_graph<T>& G, std::vect
                 }
             }
             
-            // Se il prodotto scalare è pari a 1 
+            // Se il prodotto scalare modulo 2 è pari a 1 
             if (prodscalare % 2 == 1) {
                 // S_j = S_j XOR S_i 
                 for (size_t id = 0; id < m; ++id) {
--- eser_11/Dijkstra.hpp
+++ eser_11/Dijkstra.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <set>
 #include <map>
@@ -5,37 +7,33 @@
 #include <limits>
 #include <queue>
 #include <concepts>
+
 #include "grafo.hpp"
 #include "codastack.hpp"
 
 
 template <typename T> requires std::integral<T>
-unidirected_graph<T> dijkstra( const unidirected_graph<T>& grafo, T sorgente, const std::map<unidirected_edge<T>, int>& pesi) 
+void dijkstra( size_t n, const unidirected_graph<T>& grafo, T sorgente, const std::map<unidirected_edge<T>, double>& pesi, std::vector<double>& distanze, std::vector<T>& pred) 
 {
-    std::map<T, int> distanze;
-	unidirected_graph<T> albero_cammini_minimi;
-
-    std::map<T, T> padre;
-   
-
-    //Inizializziamo le distanze a infinito (di meno)
-    for (const T& nodo : grafo.all_nodes()) {
-        distanze[nodo] = std::numeric_limits<int>::max();
-    }
-    distanze[sorgente] = 0;
-
+	distanze.assign(n,std::numeric_limits<double>::infinity());
+	pred.assign(n, T(-1));
 	
-	// coppia {distanza, nodo}
-    std::priority_queue<std::pair<int, T>, std::vector<std::pair<int, T>>, std::greater<std::pair<int, T>>> pq;
+	pred[sorgente]=sorgente;
+    distanze[sorgente]=0.0;
+	
+	
+	// coppia {distanza, nodo} - coda con priorità dove gli elementi sono i vertici e le proprità sono le distanze tra nodi
+    std::priority_queue<std::pair<double, T>, std::vector<std::pair<double, T>>, std::greater<std::pair<double, T>>> pq;
 
     pq.push({distanze[sorgente], sorgente});
 
     while (!pq.empty()) {
 		
-        std::pair<int, T> dequeue = pq.top();
+        std::pair<double, T> dequeue = pq.top();
 		pq.pop();
 		
-        int d = dequeue.first;
+		//distanza dalla sorgente di u
+        double d = dequeue.first;
         T u = dequeue.second;
 
         if (d > distanze[u]) continue;
@@ -43,20 +41,15 @@ unidirected_graph<T> dijkstra( const unidirected_graph<T>& grafo, T sorgente, co
         std::set<T> vicini = grafo.neighbors(u);
         for (const T& v : vicini) {
             unidirected_edge<T> arco(u, v);
-            int peso_arco = pesi.at(arco); 
-
+            double peso_arco = pesi.at(arco); 
+			
+			//se la distanza di u dalla sorgente + il peso (u,v) è minore della distanza di v dalla sorgente aggiorno pred e distanze
             if (distanze[u] + peso_arco < distanze[v]) {
                 distanze[v] = distanze[u] + peso_arco;
-                padre[v] = u;
+                pred[v] = u;
                 pq.push({distanze[v], v}); 
             }
         }
-    }
-
-    for (const auto& [figlio, genitore] : padre) {
-        albero_cammini_minimi.add_edge(genitore, figlio);
-    }
-
-    return albero_cammini_minimi;
+	}
 }
 
